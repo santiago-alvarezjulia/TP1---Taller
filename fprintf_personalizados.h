d577884297ba0243d1c5db5c80f5084626586f4a
santiago-alvarezjulia <santiago.alvarezjulia@gmail.com>
2018-04-11 16:18:42 -0300

    Delete fprintf

--- fprintf_personalizados.h
+++
@@ -1,8 +0,0 @@
-#ifndef FPRINTF_PERSONALIZADOS_H
-#define FPRINTF_PERSONALIZADOS_H
-
-void fprintf_hexadecimal_stdout(const char* string, size_t largo_string);
-
-void fprintf_hexadecimal_stderr(const char* string, size_t largo_string);
-
-#endif
